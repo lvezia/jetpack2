b687be70f2a0545e0506f92640208ccb248839e2
Louis Vezia <vezia_l@epitech.eu>
2016-07-07 19:28:20 +0200

    Add main and Makefile

--- client/src/main.c
+++ client/src/main.c
@@ -1,15 +1,30 @@
 /*
-** main.c for jetpack2Tek3 in /home/david/dev/syn_pool/jetpack2Tek3/client/src/
+** main.c for jetpack2Tek3 in /home/vezia_l/rendu/jetpack2Tek3/client/src/
 **
-** Made by David Calo
-** Login   <calo_d@epitech.eu>
+** Made by Louis Vezia
+** Login   <vezia_l@epitech.eu>
 **
-** Started on  Thu Jul  7 10:26:40 2016 David Calo
-** Last update Thu Jul  7 10:31:06 2016 David Calo
+** Started on  Thu Jul  7 11:09:35 2016 Louis Vezia
+** Last update	Thu Jul 07 19:22:04 2016 Louis Vezia
 */
 
-int	main()
+#include "client.h"
+
+int	my_put_error(char *str)
 {
-  /* code */
-  return 0;
+  write(2, str, strlen(str));
+  write(2, "\n", 1);
+  return (84);
+}
+
+int		main(int ac, char **av)
+{
+  t_client	client;
+
+  if (ac != 5)
+    return (my_put_error("USAGE : ./clientJ2T3 -h <ip> -p <port>"));
+  pars_arg(ac, av, &client);
+  init_socket(&client);
+  run(&client);
+  return (0);
 }
