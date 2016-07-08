f06f3e733d800b9482173b5fa9e71431c9f0d8db
David Calo <david.calo@epitech.eu>
2016-07-08 21:05:59 +0200

    Add signal handling

--- server/src/main.c
+++ server/src/main.c
@@ -5,34 +5,63 @@
 ** Login   <calo_d@epitech.eu>
 **
 ** Started on  Thu Jul  7 10:26:40 2016 David Calo
-** Last update Fri Jul  8 10:09:29 2016 David Calo
+** Last update Fri Jul  8 20:58:27 2016 David Calo
 */
 
 // #include <stdio.h>
 
 #include "server.h"
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static t_server	s;
+
+static void	signal_handler(int sig)
+{
+  dprintf(2, "[server] Received signal %d\n", sig);
+  if (sig == 2)
+    exit(EXIT_SUCCESS);
+}
+
+void	server_free()
+{
+  size_t	i;
+
+  for (i = 1; i < list_size(s.client); list_remove(s.client, i))
+    close(list_get(s.client, i)->fd);
+  close(s.client->fd);
+  free(s.client);
+  free(s.map);
+}
 
 int	main(int ac, char const *av[])
 {
-  // t_bool	run;
-  t_server	server;
+  struct sigaction	sig;
+  t_bool	run;
+  size_t	i;
+  size_t	j;
 
-  arg_read(ac, av, &server);
-  process_map(&server);
-  set_socket(&server);
-  bind_listen_socket(&server);
-  // run = TRUE;
-  // while (run)
-  //   {
-      // set_fds();
-      // select();
-      // socket_server();
-      // while (clients)
-      // {
-      //   socket_client();
-      // }
+  sig.sa_handler = &signal_handler;
+  if (sigaction(SIGINT, &sig, NULL) || sigaction(SIGPIPE, &sig, NULL))
+    return (xperror("sigaction"));
+  if (atexit(server_free))
+    return (xputerror("atexit: failed"));
+  arg_read(ac, av, &s);
+  // process_map(&s);
+  if (server_init(&s))
+    return (84);
+  run = TRUE;
+  while (run)
+    {
+      server_select(&s);
+      for (i = 0; i < list_size(s.client); i++)
+	for (j = 0; j < 2; j++)
+	  if (FD_ISSET(list_get(s.client, i)->fd, &s.fds[j]) &&
+	      list_get(s.client, i)->fn[j](&s))
+	    client_close(s.client, i--);
       // run = update_game();
-    // }
-  // free_all();
-  return 0;
+    }
+  return (0);
 }
