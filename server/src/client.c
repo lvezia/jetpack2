b29d4f057fdb38cebcee67f29f4ed513e7e7a9b2
David Calo <david.calo@epitech.eu>
2016-07-12 19:05:03 +0200

    Fix READ LINE BY LINE

--- server/src/client.c
+++ server/src/client.c
@@ -5,7 +5,7 @@
 ** Login   <calo_d@epitech.eu>
 **
 ** Started on  Fri Jul  8 11:17:29 2016 David Calo
-** Last update Tue Jul 12 11:38:16 2016 David Calo
+** Last update Tue Jul 12 19:01:45 2016 David Calo
 */
 
 #include "server.h"
@@ -20,6 +20,7 @@
 int	client_read(t_server *s, size_t n)
 {
   char		line[BUFFER_SIZE];
+  char		*tmp;
   t_fd		*cl;
   ssize_t	r;
 
@@ -34,10 +35,13 @@ int	client_read(t_server *s, size_t n)
   line[r - 1] = 0;
   if (*cl->rbuf)
     memset(cl->rbuf, 0, strlen(cl->rbuf));
-  if (!parser(cl, line))
+  for (tmp = line; (tmp = strtok(tmp, "\n")); tmp = NULL)
     {
-      strcpy(cl->rbuf, line);
-      printf("[%d] \"%s\"\n", cl->fd, cl->rbuf);
+      if (!parser(cl, tmp))
+	{
+	  strcpy(cl->rbuf, tmp);
+	  printf("[%d] \"%s\"\n", cl->fd, cl->rbuf);
+	}
     }
   return (SUCCESS);
 }
@@ -51,8 +55,8 @@ int	client_write(t_server *s, size_t n)
     {
       if (!strcmp(cl->wbuf, "MAP"))
 	sprintf(cl->wbuf, "MAP %d %d %s", s->game.mx, s->game.my, s->game.map);
-      if (write(cl->fd, cl->wbuf, strlen(cl->wbuf)) < 1 ||
-	  write(cl->fd, "\n", 1) < 1)
+      strcat(cl->wbuf, "\n");
+      if (write(cl->fd, cl->wbuf, strlen(cl->wbuf)) < 1)
 	return (errno != ECONNRESET ? xperror("write") : FAIL);
       memset(cl->wbuf, 0, strlen(cl->wbuf));
     }
